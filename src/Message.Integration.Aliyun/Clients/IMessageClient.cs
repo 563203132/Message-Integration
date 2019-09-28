b3103cf03e4c470355cb4bceea9b061c987ff3d9
563203132 <563203132@qq.com>
2019-09-28 21:06:11 +0800

    Add project files.

---
+++ src/Message.Integration.Aliyun/Clients/IMessageClient.cs
@@ -0,0 +1,7 @@
+﻿namespace Message.Integration.Aliyun.Clients
+{
+    public interface IMessageClient
+    {
+        void Send(string accessKeyId, string secret, string message);
+    }
+}
---
+++ src/Message.Integration.Aliyun/Clients/MessageClient.cs
@@ -0,0 +1,40 @@
+﻿using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Exceptions;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Profile;
+using System;
+
+namespace Message.Integration.Aliyun.Clients
+{
+    public class MessageClient : IMessageClient
+    {
+        public void Send(string accessKeyId, string secret, string message)
+        {
+            IClientProfile profile = DefaultProfile.GetProfile("", accessKeyId, secret);
+            DefaultAcsClient client = new DefaultAcsClient(profile);
+            CommonRequest request = new CommonRequest();
+            request.Method = MethodType.POST;
+            request.Domain = "dysmsapi.aliyuncs.com";
+            request.Version = "2017-05-25";
+            request.Action = "SendSms";
+            // request.Protocol = ProtocolType.HTTP;
+            request.AddQueryParameters("PhoneNumbers", "18739974753");
+            request.AddQueryParameters("SignName", "寻医问药");
+            request.AddQueryParameters("TemplateCode", "SMS_174991632");
+            request.AddQueryParameters("TemplateParam", message);
+            try
+            {
+                CommonResponse response = client.GetCommonResponse(request);
+                Console.WriteLine(System.Text.Encoding.Default.GetString(response.HttpResponse.Content));
+            }
+            catch (ServerException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (ClientException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
